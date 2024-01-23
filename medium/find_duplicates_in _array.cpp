1017fb68d38a7e6f56ebbada8fa5b7936c3a64e1
Abhishek kumar singh <115399497+kabhishekkumarsingh@users.noreply.github.com>
2024-01-23 15:47:26 +0530

    Create find_duplicates_in _array.cpp

---
+++ medium/find_duplicates_in _array.cpp
@@ -0,0 +1,25 @@
+
+class Solution{
+  public:
+    vector<int> duplicates(long long arr[], int n) {
+        // code here
+       vector<int>ans;
+      
+      for(int i=0;i<n;i++){
+          int index=arr[i]%n;
+          arr[index]+=n;
+      }
+      for(int i=0;i<n;i++){
+          if((arr[i]/n)>=2){
+              
+              ans.push_back(i);
+          }
+      }
+      if(ans.size()==0) {
+          
+          return {-1};
+      }
+       return ans;
+    }
+};
+
