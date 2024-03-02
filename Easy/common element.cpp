2e3c194e468f2814dab94508bf03b38bf387061e
Abhishek kumar singh <115399497+kabhishekkumarsingh@users.noreply.github.com>
2024-03-02 19:44:30 +0530

    Create common element.cpp

---
+++ Easy/common element.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution{
+    public:
+        vector<int> common_element(vector<int>v1,vector<int>v2)
+    {
+        // Your code here
+        unordered_map<int,int>mp;
+        for(auto i:v1) mp[i]++;
+        v1.clear();
+        for(auto i:v2 ){
+            if(mp[i]){
+                mp[i]--;
+                v1.push_back(i);
+            }
+        }
+        sort(v1.begin(),v1.end());
+        return v1;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        vector<int>v1(n,0);
+        for(int i=0;i<n;i++)
+        {
+            cin>>v1[i];
+        }
+        int m;
+        cin>>m;
+        vector<int>v2(m,0);
+        for(int i=0;i<m;i++)
+        {
+            cin>>v2[i];
+        }
+        Solution ob;
+        vector<int>result;
+        result=ob.common_element(v1,v2);
+        for(auto i:result)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        
+    }
+}
+// } Driver Code Ends
