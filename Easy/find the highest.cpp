885ee84e9dad441ae66e284cf92d6177d0b14a1f
Abhishek kumar singh <115399497+kabhishekkumarsingh@users.noreply.github.com>
2024-03-03 14:11:46 +0530

    Create find the highest.cpp

---
+++ Easy/find the highest.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        // Code here.
+        int l=0;
+        int h=a.size()-1;
+        if(a[h]>a[h-1]) return a[h];
+        for(int i=0;i<=h;i++){
+            if(a[i]>a[i+1]) return a[i];
+        }
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
