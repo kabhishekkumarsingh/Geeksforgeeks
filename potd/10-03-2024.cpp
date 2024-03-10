fce5e3ccf68447e5fe4826474f015beda73c9bc1
Abhishek kumar singh <115399497+kabhishekkumarsingh@users.noreply.github.com>
2024-03-10 08:13:13 +0530

    Create 10-03-2024.cpp

---
+++ potd/10-03-2024.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string s) {
+	    // code here
+	   unordered_map<char, int> exists;
+    string ans = "";
+    int n=s.length();
+    for (int i = 0; i < n; i++) {
+        if (exists.find(s[i]) == exists.end()) {
+            ans.push_back(s[i]);
+            exists[s[i]]++;
+        }
+    }
+    return ans;
+	    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
