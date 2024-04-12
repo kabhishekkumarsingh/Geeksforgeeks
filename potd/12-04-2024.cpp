6ee12b9d9a666a12634725f24735c6c5e0a8b2b5
Abhishek kumar singh <115399497+kabhishekkumarsingh@users.noreply.github.com>
2024-04-12 06:27:22 +0530

    Create 12-04-2024.cpp

---
+++ potd/12-04-2024.cpp
@@ -0,0 +1,16 @@
+long long pairAndSum(int n, long long arr[]) {
+        // code here
+         long long ans = 0;
+
+        for(int i = 0;i < 32; i++) {
+            long long count = 0;
+            for(int j = 0;j < n; j++) {
+                if(arr[j] & (1<<i))
+                    count++;
+            }
+
+            ans += 1ll*(count*(count-1)/2)*(1ll<<i);
+        }
+
+        return ans;
+    }
